12126087aaed9fd881520e48af35a148a606c3b2
Nate Schultz <ngs32@drexel.edu>
2022-07-05 23:09:18 -0400

    speed up video

--- mp4.py
+++ mp4.py
@@ -1,3 +1,25 @@
 import os
+from moviepy.editor import VideoFileClip
+import moviepy.video.fx.all as vfx
 
+# write initial video
 os.system("ffmpeg -r 1 -i images/result%01d.jpg -vcodec mpeg4 -y movie.mp4")
+
+# speed up video
+
+in_loc = 'movie.mp4'
+out_loc = 'noise.mp4'
+
+# Import video clip
+clip = VideoFileClip(in_loc)
+print("fps: {}".format(clip.fps))
+
+# Modify the FPS
+clip = clip.set_fps(clip.fps * 10)
+
+# Apply speed up
+final = clip.fx(vfx.speedx, 10)
+print("fps: {}".format(final.fps))
+
+# Save video clip
+final.write_videofile(out_loc)
